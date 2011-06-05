bc012fa85c186bbb4d456e74824b862509b0fe36
Jakub Klímek <718827+jakubklimek@users.noreply.github.com>
2011-06-05 23:27:42 +0000

    PSM delete subtree update, support for multiple subtrees

--- View/Commands/PSM/guiDeleteSubtree.cs
+++ View/Commands/PSM/guiDeleteSubtree.cs
@@ -2,6 +2,7 @@ using System;
 using Exolutio.Dialogs;
 using Exolutio.ResourceLibrary;
 using Exolutio.Model.PSM;
+using Exolutio.Model;
 using System.Linq;
 using System.Collections.Generic;
 using Exolutio.Controller.Commands.Complex.PSM;
@@ -20,32 +21,62 @@ namespace Exolutio.View.Commands.PSM
             if (Current.ActiveDiagramView.IsSelectedComponentOfType(typeof(PSMSchemaClass))
                 || Current.ActiveDiagramView.IsSelectedComponentOfType(typeof(PSMAttribute))) return false;
 
-            if (Current.ActiveDiagramView.GetSelectedComponents().Count() != 1) return false;
+            if (Current.ActiveDiagramView.GetSelectedComponents().Count() > 0) return true;
 
-            return true;
+            return false;
 
         }
 
         public override void Execute(object parameter)
         {
-            PSMComponent component = Current.ActiveDiagramView.GetSingleSelectedComponentOrNull() as PSMComponent;
+            IEnumerable<PSMComponent> components = Current.ActiveDiagramView.GetSelectedComponents().Cast<PSMComponent>();
+            bool found = true;
+            IEnumerable<PSMComponent> current = components;
+            while (found)
+            {
+                found = false;
+                List<PSMComponent> next = new List<PSMComponent>();
+                foreach (PSMComponent c in current)
+                {
+                    if (current.Any(co => co != c && c.IsDescendantFrom(co))) found = true;
+                    else next.Add(c);
+                }
+                current = next;
+            }
 
-            if (!(component is PSMAssociation)) component = (component as PSMAssociationMember).ParentAssociation;
-                cmdDeletePSMAssociationRecursive da = new cmdDeletePSMAssociationRecursive(Current.Controller) { AssociationGuid = component };
-            da.Execute();
+            IEnumerable<PSMAssociation> associations = current.Where(c => c is PSMAssociation).Cast<PSMAssociation>();
+            IEnumerable<PSMAssociationMember> associationMembers = current.Where(c2 => (!(c2 is PSMSchemaClass) && (c2 is PSMAssociationMember))).Cast<PSMAssociationMember>();
+            IEnumerable<PSMAssociationMember> roots = associationMembers.Where(am => am.ParentAssociation == null);
+            IEnumerable<PSMAssociation> nonrootAssociations = associationMembers.Where(am => am.ParentAssociation != null).Select(am2 => am2.ParentAssociation);
+            IEnumerable<PSMAttribute> attributes = current.Where(c => c is PSMAttribute).Cast<PSMAttribute>();
+            MacroCommand macro = new MacroCommand(Current.Controller);
+            foreach (PSMAssociation a in associations.Union(nonrootAssociations))
+            {
+                macro.Commands.Add(new cmdDeletePSMAssociationRecursive(Current.Controller) { AssociationGuid = a });
+            }
+            foreach (PSMAssociationMember am in roots)
+            {
+                macro.Commands.Add(new cmdDeletePSMAssociationMemberRecursive(Current.Controller) { AssociationMemberGuid = am });
+            }
+            foreach (PSMAttribute a in attributes)
+            {
+                macro.Commands.Add(new cmdDeletePSMAttribute(Current.Controller) { AttributeGuid = a });
+            }
+            
+            macro.Execute();
         }
 
         public override string Text
         {
             get
             {
-                return "Delete subtree";
+                return "Delete subtrees";
             }
         }
 
         public override string ScreenTipText
         {
-            get { return "Delete PSM subtree"; }
+            get { return "Delete PSM subtrees"; }
         }
 
         public override System.Windows.Media.ImageSource Icon
