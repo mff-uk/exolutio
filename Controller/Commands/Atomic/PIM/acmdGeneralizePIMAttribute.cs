d16fb5c3d3605bad0a6d8aabc02e8bb2133677b2
Jakub Klímek <718827+jakubklimek@users.noreply.github.com>
2012-03-22 09:19:00 +0000

    Controller: Inheritance: Generalize PIM attribute propagation and necessary complex commands

--- Controller/Commands/Atomic/PIM/acmdGeneralizePIMAttribute.cs
+++ Controller/Commands/Atomic/PIM/acmdGeneralizePIMAttribute.cs
@@ -1,6 +1,10 @@
 ﻿using System;
 using Exolutio.Model.PIM;
 using Exolutio.Model;
+using Exolutio.Model.PSM;
+using System.Collections.Generic;
+using System.Linq;
+using Exolutio.Controller.Commands.Complex.PSM;
 
 namespace Exolutio.Controller.Commands.Atomic.PIM
 {
@@ -59,9 +63,33 @@ namespace Exolutio.Controller.Commands.Atomic.PIM
             return OperationResult.OK;
         }
 
-        /*internal override PropagationMacroCommand PrePropagation()
+        internal override PropagationMacroCommand PrePropagation()
         {
-            
-        }*/
+            PIMAttribute pimAttribute = Project.TranslateComponent<PIMAttribute>(attributeGuid);
+            PIMClass oldPIMClass = pimAttribute.PIMClass;
+            PIMClass newPIMClass = oldPIMClass.GeneralizationAsSpecific.General;
+            List<PSMAttribute> psmAttributes = pimAttribute.GetInterpretedComponents().Cast<PSMAttribute>().ToList();
+
+            PropagationMacroCommand command = new PropagationMacroCommand(Controller) { CheckFirstOnlyInCanExecute = true };
+            command.Report = new CommandReport("Pre-propagation (generalize PIM attribute)");
+
+            foreach (PSMAttribute a in psmAttributes)
+            {
+                IEnumerable<PSMClass> generals = a.PSMClass.NearestInterpretedClass().GetGeneralClasses();
+                if (generals.Any(c => c.Interpretation == newPIMClass))
+                {
+                    if (a.PSMClass.Interpretation == null)
+                    {
+                        //classa neinterpret, mozna i s impl.inheritance
+                        command.Commands.Add(new cmdMovePSMAttribute(Controller) { AttributeGuid = a, ClassGuid = a.PSMClass.NearestInterpretedClass() });
+                    }
+
+                    //whether the class uses implicit inheritance or not (a.PSMClass.Interpretation == oldPIMClass) we move it to the PSMClass, whose interpretation is newPIMClass
+                    command.Commands.Add(new cmdGeneralizePSMAttribute(Controller) { AttributeGuid = a, PSMClassGuid = generals.First(c => c.Interpretation == newPIMClass) });
+                }
+            }
+
+            return command;            
+        }
     }
 }
--- Controller/Commands/CommandErrors.Designer.cs
+++ Controller/Commands/CommandErrors.Designer.cs
@@ -61,6 +61,15 @@ namespace Exolutio.Controller.Commands {
         }
         
         /// <summary>
+        ///   Looks up a localized string similar to Cannot generalize, interpretation would be violated..
+        /// </summary>
+        internal static string CMDERR_CANNOT_GENERALIZE_INTERPRET_ERR {
+            get {
+                return ResourceManager.GetString("CMDERR_CANNOT_GENERALIZE_INTERPRET_ERR", resourceCulture);
+            }
+        }
+        
+        /// <summary>
         ///   Looks up a localized string similar to Cannot set a class as final because it is a general class. Delete generalizations first..
         /// </summary>
         internal static string CMDERR_CANNOT_SET_FINAL_GENERALIZATIIONS_EXIST {
