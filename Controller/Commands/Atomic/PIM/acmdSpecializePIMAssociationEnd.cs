efb5613cce3135d688041f6ef795b61e161c6af2
Jakub Klímek <718827+jakubklimek@users.noreply.github.com>
2012-04-27 08:24:44 +0000

    Propagation of SpecializePIMAssociationEnd, SpecializePIMAttribute propagation fix

--- Controller/Commands/Atomic/PIM/acmdSpecializePIMAssociationEnd.cs
+++ Controller/Commands/Atomic/PIM/acmdSpecializePIMAssociationEnd.cs
@@ -1,6 +1,11 @@
 ﻿using System;
 using Exolutio.Model.PIM;
 using Exolutio.Model;
+using Exolutio.Model.PSM;
+using System.Collections.Generic;
+using System.Linq;
+using Exolutio.Controller.Commands.Complex.PSM;
+using Exolutio.Controller.Commands.Atomic.PSM;
 
 namespace Exolutio.Controller.Commands.Atomic.PIM
 {
@@ -60,9 +65,55 @@ namespace Exolutio.Controller.Commands.Atomic.PIM
             return OperationResult.OK;
         }
 
-        /*internal override PropagationMacroCommand PrePropagation()
+        internal override PropagationMacroCommand PrePropagation()
         {
-            
-        }*/
+            PIMAssociationEnd pimAssociationEnd = Project.TranslateComponent<PIMAssociationEnd>(associationEndGuid);
+            PIMClass generalPIMClass = pimAssociationEnd.PIMClass;
+            PIMClass specialPIMClass = Project.TranslateComponent<PIMClass>(specialClassGuid);
+
+            IEnumerable<PSMAssociation> psmAssociations = pimAssociationEnd.PIMAssociation.GetInterpretedComponents().Cast<PSMAssociation>();
+            List<PSMAssociation> psmAssociationsNormalDirection = psmAssociations.Where(a => a.InterpretedAssociationEnd != pimAssociationEnd).ToList();
+            List<PSMAssociation> psmAssociationsOppositeDirection = psmAssociations.Where(a => a.InterpretedAssociationEnd == pimAssociationEnd).ToList();
+
+            PropagationMacroCommand command = new PropagationMacroCommand(Controller) { CheckFirstOnlyInCanExecute = true };
+            command.Report = new CommandReport("Pre-propagation (specialize PIM association end)");
+
+            foreach (PSMAssociation a in psmAssociationsNormalDirection)
+            {
+                if (a.Parent.Interpretation == null)
+                {
+                    //class without interpretation, maybe including impl.inheritance
+                    command.Commands.Add(new cmdReconnectPSMAssociation(Controller) { AssociationGuid = a, NewParentGuid = a.Parent.NearestInterpretedClass() });
+                }
+
+                IEnumerable<Tuple<PSMClass, IEnumerable<PSMClass>>> paths = a.Parent.NearestInterpretedClass().GetSpecialClassesWithPaths();
+                if (paths.Any(p => p.Item1.Interpretation == specialPIMClass))
+                //1) there is special PIMClass counterpart -> move there
+                {
+                    IEnumerable<PSMClass> path = paths.First(p => p.Item1.Interpretation == specialPIMClass).Item2;
+                    foreach (PSMClass c in path)
+                    {
+                        command.Commands.Add(new acmdSpecializePSMAssociation(Controller, a, c));
+                    }
+                }
+                else
+                //2) There is none... create. TODO: FIX: multiple attributes in one class => multiple classes
+                {
+                    Guid newClassGuid = Guid.NewGuid();
+                    command.Commands.Add(new acmdNewPSMClass(Controller, a.PSMSchema) { ClassGuid = newClassGuid });
+                    command.Commands.Add(new acmdRenameComponent(Controller, newClassGuid, specialPIMClass.Name));
+                    command.Commands.Add(new acmdSetPSMClassInterpretation(Controller, newClassGuid, specialPIMClass));
+                    command.Commands.Add(new acmdNewPSMGeneralization(Controller, a.Parent.NearestInterpretedClass(), newClassGuid, a.PSMSchema));
+                    command.Commands.Add(new acmdSpecializePSMAssociation(Controller, a, newClassGuid));
+                }
+            }
+
+            foreach (PSMAssociation a in psmAssociationsOppositeDirection)
+            {
+                command.Commands.Add(new cmdDeletePSMAssociation(Controller) { AssociationGuid = a });
+            }
+
+            return command;                 
+        }
     }
 }
